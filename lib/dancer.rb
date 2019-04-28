385aa87e97667f625bd4640d3eb87069985c07ac
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-28 22:08:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,16 +2,13 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  class Dancer
     extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
-  end
+
  attr_accessor :name
 
  def initialize(name)
    @name = name
  end
-
- extend MetaDancing
 
 end
